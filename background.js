9a66cce8984cd793ea9f81f5cdb1dc71f64e30b0
DJTB <duncan.bay@gmail.com>
2015-11-28 23:34:16 +1100

    remove other files - img is here for inline readme reference on master

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.extension.onMessage.addListener(function (msg, sender, sendResponse) {
-    //Set Content
-    document.getElementById("tmpClipboard").value = msg.text;
-    //Get Input Element
-    document.getElementById("tmpClipboard").select();
-
-    //Copy Content
-    document.execCommand("Copy", false, null);
-});
