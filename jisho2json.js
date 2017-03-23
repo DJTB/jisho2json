f0a53050be1ae08264927e6e537ea52268c23c6b
Duncan Bay <duncan.bay@gmail.com>
2017-03-23 21:50:06 +0100

    feat(toast): add css styles to doc head, better indentation

--- jisho2json.js
+++ jisho2json.js
@@ -1,66 +1,114 @@
 /* eslint-disable no-console, no-undef */
+// TODO: remove jQuery dependency, modern Chrome's api is solid
 
-/* Unfortunately there are few unique classes and no ids, which makes targeting specific info rather unwieldy and brittle. If any of the classes or structure changes this will have to be adjusted manually. */
+const styles = `
+   /* highlight clickable entries on hover */
+  .concept_light {
+    transition: all .5s ease-in
+  }
+  .concept_light:hover {
+    transition: all .3s ease-out;
+    box-shadow: 0 0 4px 0 cornflowerblue;
+    cursor: pointer;
+  }
+  /* notification we will add to body */
+  .toast {
+    position: fixed;
+    top: .5rem;
+    right: .5rem;
+    bottom: .5rem;
+    left: .5rem;
+    padding: .5rem;
+    max-width: calc(100vw - 1rem);
+    max-height: calc(100vh - 1rem);
+    overflow: auto;
+    font-size: 1.2rem;
+    opacity: 0;
+    margin: 0 .3rem;
+    border: 4px solid gainsboro;
+    border-radius: 5px;
+    background-color: cornflowerblue;
+    text-transform: capitalize;
+    color: whitesmoke;
+    cursor: pointer;
+    z-index: 100; /* jisho overlay is at 95 already */
+    /* <pre> formatting */
+    white-space: pre-wrap;
+    word-wrap: break-word;
+    transition: opacity .35s ease-in;
+    /* disable clicks */
+    pointer-events: none;
+  }
+  .toast.--isVisible {
+    transition: opacity .25s ease-out;
+    pointer-events: initial;
+    opacity: .95;
+  }
+`;
 
 // blastoff!
 ready(init);
 
 function init() {
   console.info('jisho2json loaded');
+  let toaster; // notification element
+  let pendingFade; // cancellable delayed fadeOut animation setTimeout timer
 
-  renderToast()
-    .then((toast) => {
-      let pendingFade;
-      // allow user to dismiss toast
-      toast.addEventListener('click', () => fadeOut(toast));
+  appendStyleToHead(styles);
 
-      // highlight clickable entries
-      $('.concept_light').hover(addHighlight, removeHighlight);
+  renderToast().then((toast) => {
+    toaster = toast;
+    toaster.addEventListener('click', () => toaster.classList.remove('--isVisible'));
+  }).catch(console.error);
 
-      // parse and copy entry to clipboard
-      $('body').on('click', '.concept_light', function onEntryClick() {
-        const obj = buildJRE($(this));
-        sendText(obj); // stringified JSON
-        console.info('Copied:\n', (obj));
-        toast.textContent = obj; // eslint-disable-line no-param-reassign
 
-        // animate and hold onto fadeout timer reference to allow reset
-        fadeIn(toast, 0.95);
-        clearTimeout(pendingFade);
-        pendingFade = setTimeout(() => fadeOut(toast), 5000);
-      });
-    })
-    .catch(console.error);
+  // parse and copy entry to clipboard
+  document.querySelector('body').addEventListener('click', ({ target }) => {
+    const desiredTargetSelector = '.concept_light';
+    const limit = document.querySelector('#primary');
+    const entry = getParent(target, desiredTargetSelector, limit);
+
+    if (entry) {
+      const entryJSON = JSON.stringify(buildJRE($(entry)), null, 2);
+      console.info('Copied:\n', (entryJSON));
+      sendText(entryJSON); // copy to clipboard
+
+      toaster.textContent = entryJSON; // eslint-disable-line no-param-reassign
+      toaster.classList.add('--isVisible');
+      clearTimeout(pendingFade);
+      pendingFade = setTimeout(() => toaster.classList.remove('--isVisible'), 5000);
+    }
+  });
+}
+
+function getParent(el, selector, limit) {
+  if (el.nodeType === 1 /* it's an element! */ && el.matches(selector)) {
+    return el;
+  }
+  if (el === limit || !el.parentNode) {
+    return false;
+  }
+  return el.parentNode ? getParent(el.parentNode, selector) : false;
+}
+
+function appendStyleToHead(style) {
+  const element = document.createElement('style');
+  element.setAttribute('type', 'text/css');
+  element.appendChild(document.createTextNode(style));
+  document.head.appendChild(element);
 }
 
 function renderToast() {
   return new Promise((resolve, reject) => {
     const toast = document.createElement('pre');
     Object.assign(toast, {
-      id: 'toast',
+      className: 'toast',
       textContent: 'jisho2json loaded',
     });
-    Object.assign(toast.style, {
-      position: 'fixed',
-      top: '.5rem',
-      right: '.5rem',
-      padding: '2rem',
-      maxWidth: 'calc(100vw - 1rem)',
-      fontSize: '1.2rem',
-      opacity: '0',
-      margin: '0 .3rem',
-      borderRadius: '5px',
-      backgroundColor: 'cornflowerblue',
-      textTransform: 'capitalize',
-      color: 'whitesmoke',
-      cursor: 'pointer',
-      zIndex: '100', // jisho overlay is 95 >_<
-      // <pre> formatting
-      whiteSpace: 'pre-wrap',
-      wordWrap: 'break-word',
-    });
-    document.body.append(toast);
-    const toastEl = document.getElementById('toast');
+
+    document.body.appendChild(toast);
+
+    const toastEl = document.querySelector('.toast');
     return toastEl ? resolve(toastEl) : reject(new Error('Toast element not added to body'));
   });
 }
@@ -73,50 +121,7 @@ function ready(callback) {
   }
 }
 
-function fadeIn(el = {}, targetOpacity = 1, speed = 400) {
-  let opacity = +window.getComputedStyle(el).opacity;
-  let time = +new Date();
-  if (opacity >= targetOpacity) return;
-
-  function tick() {
-    opacity += ((new Date() - time) / speed);
-    time = +new Date();
-    el.style.opacity = opacity; // eslint-disable-line no-param-reassign
-
-    if (opacity < targetOpacity) window.requestAnimationFrame(tick);
-  }
-
-  tick();
-}
-
-function fadeOut(el = {}, targetOpacity = 0, speed = 400) {
-  let opacity = +el.style.opacity;
-  let time = +new Date();
-  if (opacity <= targetOpacity) return;
-
-  function tick() {
-    opacity -= ((new Date() - time) / speed);
-    time = +new Date();
-    el.style.opacity = opacity; // eslint-disable-line no-param-reassign
-
-    if (opacity > targetOpacity) window.requestAnimationFrame(tick);
-  }
-
-  tick();
-}
-
-function addHighlight() {
-  $(this).css({
-    boxShadow: '0 0 4px 0 cornflowerblue',
-    cursor: 'pointer',
-  });
-}
-
-function removeHighlight() {
-  $(this).css({
-    boxShadow: 'none',
-  });
-}
+/* Unfortunately there are few unique classes and no ids, which makes targeting specific info rather unwieldy and brittle. If any of the classes or structure changes this will have to be adjusted manually. */
 
 function parseKana(el) {
   return (el.find('.f-dropdown li:nth-of-type(2)').text().match(/(?!\b)\W+$/) || [])[0];
@@ -167,7 +172,7 @@ function parseInfo(el) {
 
 function buildJRE($entry) {
   const characters = parseKanji($entry);
-  return JSON.stringify({
+  return {
     common: !!$entry.find('.concept_light-common').length,
     jlpt: $entry.find('.concept_light-tag:contains("JLPT")').text() || null,
     ja: {
@@ -175,7 +180,7 @@ function buildJRE($entry) {
       readings: parseKana($entry) || characters, // sometimes no kana if characters were hiragana to begin with
     },
     en: parseMeanings($entry),
-  });
+  };
 }
 
 function sendText(text) {
