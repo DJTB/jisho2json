34074dfad3e831a19f5ef0df0613b89c67df739d
Duncan Bay <duncan.bay@gmail.com>
2017-03-26 01:29:26 +0100

    build: add crx chrome extension build script

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,6 @@
 const webpack = require('webpack');
 const CircularDependencyPlugin = require('circular-dependency-plugin');
 
-const UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
-const OccurrenceOrderPlugin = webpack.optimize.OccurrenceOrderPlugin;
-
-const plugins = [
-  new CircularDependencyPlugin({
-    exclude: /node_modules/, // exclude node_modules
-    failOnError: false, // show a warning when there is a circular dependency
-  }),
-  new UglifyJsPlugin({
-    minimize: true,
-    output: {
-      comments: false,
-    },
-    compressor: {
-      warnings: false,
-    },
-  }),
-  new OccurrenceOrderPlugin(),
-];
-
 const libraryName = process.env.npm_package_name;
 const outputFile = `${libraryName}.js`;
 
@@ -45,5 +25,20 @@ module.exports = {
       },
     ],
   },
-  plugins,
+  plugins: [
+    new CircularDependencyPlugin({
+      exclude: /node_modules/, // exclude node_modules
+      failOnError: false, // show a warning when there is a circular dependency
+    }),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true,
+      output: {
+        comments: false,
+      },
+      compressor: {
+        warnings: false,
+      },
+    }),
+  ],
 };
